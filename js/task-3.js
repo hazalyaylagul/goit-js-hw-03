0db6dd98765690e5535ce35ec7a198aa10173a3b
Hazal Yaylagül <78623120+hazalyaylagul@users.noreply.github.com>
2025-03-27 03:04:50 +0300

    h

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const filterArray = [];
+  for (let index = 0; index < numbers.length; index += 1) {
+    const element = numbers[index];
+    if (element > value) {
+      filterArray.push(element);
+    }
+  }
+  return filterArray;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
